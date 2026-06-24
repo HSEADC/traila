7c8451b5ef52df7559605f0bf3081af233183b6d
Irina Kondratenko <iiiii.irina.irina@gmail.com>
2026-06-24 12:00:16 +0500

    create search

--- src/javascripts/search-vanilla.js
+++ src/javascripts/search-vanilla.js
@@ -1,19 +1,83 @@
 const articles = [
   {
-    title: "Рюкзак",
-    url: "https://hseadc.github.io/traila/pages/articles/article_backpack.html",
-  },
-  {
     title: "Клещи",
-    url: "https://hseadc.github.io/traila/pages/articles/article_ticks.html",
-  },
-  {
-    title: "Клещи",
-    url: "https://hseadc.github.io/traila/pages/articles/article_ticks.html",
+    url: "https://traila.adc.ac/pages/articles/article_ticks.html",
   },
   {
     title: "Гроза",
-    url: "https://hseadc.github.io/traila/pages/articles/article_storm.html",
+    url: "https://traila.adc.ac/pages/articles/article_storm.html",
+  },
+  {
+    title: "Потерялся",
+    url: "https://traila.adc.ac/pages/articles/article_lost.html",
+  },
+  {
+    title: "Маршрут",
+    url: "https://traila.adc.ac/pages/articles/article_route.html",
+  },
+  {
+    title: "Ориентиры",
+    url: "https://traila.adc.ac/pages/articles/article_landmarks.html",
+  },
+  {
+    title: "Компас",
+    url: "https://traila.adc.ac/pages/articles/article_compass.html",
+  },
+  {
+    title: "Лесной экзамен",
+    url: "https://traila.adc.ac/pages/interactive/main_ForestExam.html",
+  },
+  {
+    title: "Помощь на тропе",
+    url: "https://traila.adc.ac/pages/interactive/main_Help.html",
+  },
+  {
+    title: "Съедобное или... нет?",
+    url: "https://traila.adc.ac/pages/interactive/main_Mushrooms.html",
+  },
+  {
+    title: "Что взять с собой?",
+    url: "https://traila.adc.ac/pages/interactive/main_ForestSet.html",
+  },
+  {
+    title: "Спокойное движение",
+    url: "https://traila.adc.ac/pages/interactive/main_CalmMovement.html",
+  },
+  {
+    title: "Что взять в лес",
+    url: "https://traila.adc.ac/pages/interactive/main_WhatToTake.html",
+  },
+  {
+    title: "Наблюдение",
+    url: "https://traila.adc.ac/pages/interactive/main_Observation.html",
+  },
+  {
+    title: "Чтение маршрута",
+    url: "https://traila.adc.ac/pages/interactive/main_Route.html",
+  },
+  {
+    title: "Куда идти?",
+    url: "https://traila.adc.ac/pages/interactive/main_WhereToGo.html",
+  },
+  {
+    title: "Озеро Бездонное",
+    url: "https://traila.adc.ac/pages/routes/bottomlessLake.html",
+  },
+  {
+    title: "Заповедный лес",
+    url: "https://traila.adc.ac/pages/routes/reserveForest.html",
+  },
+  {
+    title: "Тропарёвский лес",
+    url: "https://traila.adc.ac/pages/routes/troparevoForest.html",
+  },
+  {
+    title: "Воробьёвы горы",
+    url: "https://traila.adc.ac/pages/routes/sparrowHills.html",
+  },
+  {
+    title: "Лосиный остров",
+    url: "https://traila.adc.ac/pages/routes/losinyOstrovTrail.html",
   },
 ];
 
